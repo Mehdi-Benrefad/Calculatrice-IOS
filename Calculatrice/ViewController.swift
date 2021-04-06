d13d7319d3780929b308745589ccc1c93886ee2b
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-06 09:36:52 -0700

    Initial Commit

---
+++ Calculatrice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by Mehdi Benrefad on 06/04/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
